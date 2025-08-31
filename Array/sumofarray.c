8085f01b56be67710a5601de891270f57181939f
parvathyg17 <parvathyg05@gmail.com>
2025-08-31 22:55:45 +0530

    first commit

---
+++ Array/sumofarray.c
@@ -0,0 +1,25 @@
+/*sum of array*/
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Calculating the sum
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("The sum of the array elements is: %d\n", sum);
+
+    return 0;
+}
